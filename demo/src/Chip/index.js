46a5b842836ea534ea8664c590cc0ab67e777a8f
marcosun <marcoinbristol@gmail.com>
2018-04-26 13:39:19 +0800

    [demo] Chip demo shows label and hook delete callback

--- demo/src/Chip/index.js
+++ demo/src/Chip/index.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
 import {withStyles} from 'material-ui/styles';
-import Typography from 'material-ui/Typography';
+import Paper from 'material-ui/Paper';
 
 import {default as IBusUiChip} from 'ibuscloud-ui/Chip';
 
 const styles = (theme) => ({
-
 });
 
 /**
- * 
+ * Chip demo
  */
 @hot(module)
 @withStyles(styles)
 class Chip extends React.Component {
   /**
+   * Handle click on delete icon
+   * @param  {[type]} e [description]
+   */
+  handleDelete() {
+    alert('You clicked the delete icon.');
+  }
+
+  /**
    * @return {Component}
    */
   render() {
     return (
-      <IBusUiChip></IBusUiChip>
+      <Paper>
+        <IBusUiChip
+          label='10车队'
+          onDelete={this.handleDelete.bind(this)}
+        />
+      </Paper>
     );
   }
 }
