7020130fcd1503057c8f666538fe68847f54e9b5
marcosun <marcoinbristol@gmail.com>
2018-04-28 12:17:05 +0800

    [ButtonGroupDummy] Create a presentational component for ButtonGroup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export {default as AppFrame} from './AppFrame';
 export {default as ButtonGroup} from './ButtonGroup';
+export {default as ButtonGroupDummy} from './ButtonGroupDummy';
 export {default as Chip} from './Chip';
 export {default as ThemeProvider} from './ThemeProvider';
 export {default as Table} from './Table';
