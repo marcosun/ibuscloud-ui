38685e491b58d0c3843eb32ee775124552178b47
marcosun <marcoinbristol@gmail.com>
2018-05-01 21:23:59 +0800

    [demo] Delete 'All Check' functionalities from table demo

--- demo/src/Table/index.js
+++ demo/src/Table/index.js
@@ -100,7 +100,6 @@ export default class table extends React.Component {
     ];
 
     this.state = {
-      isAllChecked: false,
       currentRow: {},
       checkedRows: [],
     };
@@ -119,21 +118,10 @@ export default class table extends React.Component {
   }
 
   /**
-   * @param  {Object} event
-   */
-  onAllCheckedChange(event) {
-    this.setState({
-      ...this.state,
-      isAllChecked: event.target.checked,
-    });
-  }
-
-  /**
    * @return {Component}
    */
   render() {
     const {
-      isAllChecked,
       currentRow,
       checkedRows,
     } = this.state;
@@ -141,14 +129,12 @@ export default class table extends React.Component {
     return (
       <div style={{fontSize: '16px'}}>
         <h1>Selecting</h1>
-        <div>isAllChecked: {`${isAllChecked}`}</div>
         <div>currentRow: {JSON.stringify(currentRow)}</div>
         <div>checkedRows: {JSON.stringify(checkedRows)}</div>
         <Table
           columns={this.columns}
           rows={this.data}
           onCheckedChange={this.onCheckedChange.bind(this)}
-          onAllCheckedChange={this.onAllCheckedChange.bind(this)}
         />
         <h1>Sorting & Selecting</h1>
         <Table
