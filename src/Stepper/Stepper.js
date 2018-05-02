651b251d0e4041b2ca0ca646c50f1ca87e07fd4e
marcosun <marcoinbristol@gmail.com>
2018-05-02 17:37:03 +0800

    [Stepper] Create Stepper scaffold

---
+++ src/Stepper/Stepper.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+/**
+ * 
+ */
+class Stepper extends React.Component {
+  /**
+   * @return {Component}
+   */
+  render() {
+    return (
+      <div>Stepper</div>
+    );
+  }
+}
+
+export default Stepper;
