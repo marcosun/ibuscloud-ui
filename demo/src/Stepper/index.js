cfc468c45fe694e63498503bc32b6fe77235232c
marcosun <marcoinbristol@gmail.com>
2018-05-03 16:11:20 +0800

    [demo] Complete stepper demo

--- demo/src/Stepper/index.js
+++ demo/src/Stepper/index.js
@@ -13,12 +13,26 @@ const styles = (theme) => ({
 @hot(module)
 @withStyles(styles)
 class Stepper extends React.Component {
+  steps = [{
+    name: '基本信息',
+    path: 'buttonGroup',
+  }, {
+    name: '线路规划',
+    isActive: true,
+  }, {
+    name: '发车时间',
+    path: 'chip',
+    isCompleted: true,
+  }, {
+    name: '线路预览',
+  }];
+
   /**
    * @return {Component}
    */
   render() {
     return (
-      <IBusUiStepper />
+      <IBusUiStepper steps={this.steps} />
     );
   }
 }
