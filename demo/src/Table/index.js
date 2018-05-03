0c7f3c85473f269850925cf2c0f06fd3e2e48cc7
余珩 <1281005689@qq.com>
2018-05-03 13:01:35 +0800

    [Table demo] Simulate rows and order

--- demo/src/Table/index.js
+++ demo/src/Table/index.js
@@ -13,17 +13,27 @@ export default class table extends React.Component {
    */
   constructor(props) {
     super(props);
-    
+
+    this.total = 32;
+
+    this.order = {
+      columnId: 'name',
+      orderBy: 'asc',
+    };
+
+    this.allRows = this.getAllRows(this.total);
+
     this.columns = [
       {
         id: 'name',
         label: 'Dessert (100g serving)',
-        tooltip: '第一列',
+        tooltip: 'name',
       },
       {
         id: 'calories',
         label: 'Calories',
         isNumeric: true,
+        tooltip: 'calories',
       },
       {
         id: 'fat',
@@ -40,81 +50,117 @@ export default class table extends React.Component {
       },
     ];
 
-    this.data = [
-      {
-        id: 1,
-        name: '1',
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-      },
-      {
-        id: 2,
-        name: 'cupcake',
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-      },
-      {
-        id: 3,
-        name: 'Cupcake',
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-      },
-      {
-        id: 4,
-        name: 'Gingerbread',
-        calories: 356,
-        fat: 16.0,
-        carbs: 49,
-        protein: 3.9,
-      },
-      {
-        id: 5,
-        name: 'Donut',
-        calories: 452,
-        fat: 25.0,
-        carbs: 51,
-        protein: 4.9,
-      },
-      {
-        id: 6,
-        name: 'Eclair',
-        calories: 262,
-        fat: 16.0,
-        carbs: 24,
-        protein: 6.0,
-      },
-      {
-        id: 7,
-        name: 'Frozen yoghurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-      },
-    ];
-
     this.state = {
-      currentRow: {},
-      checkedRows: [],
+      rowId: {},
+      selectedRowIds: [],
+      rowsPerPage: 5,
+      currentPage: 0,
+      rows: this.getRows(0, 5, {...this.order}),
     };
   }
 
   /**
-   * @param  {Object} currentRow
-   * @param  {Array} checkedRows
+   * Simulate table data
+   * @param  {number} total
+   * @return {Array}
    */
-  onCheckedChange(currentRow, checkedRows) {
+  getAllRows(total) {
+    return new Array(total).fill(1).map((item, index) => {
+      return {
+        id: index,
+        name: index + 1,
+        calories: (Math.random() * 100).toFixed(0),
+        fat: (Math.random() * 100).toFixed(1),
+        carbs: (Math.random() * 100).toFixed(1),
+        protein: (Math.random() * 100).toFixed(0),
+      };
+    });
+  }
+
+  /**
+   * The current page of rows data
+   * @param  {number} currentPage
+   * @param  {number} rowsPerPage
+   * @param  {Object} options
+   * @param  {string} options.orderBy
+   * @param  {string} options.columnId
+   * @return {Array}
+   */
+  getRows(currentPage , rowsPerPage, {orderBy, columnId}) {
+    const currentPageRows = this.total - currentPage * rowsPerPage >= rowsPerPage
+      ? rowsPerPage
+      : this.total - currentPage * rowsPerPage;
+
+    if (orderBy === 'asc') {
+      return [...this.allRows].sort((pre, next) => (next[columnId] > pre[columnId] ? -1 : 1))
+        .slice(currentPage * rowsPerPage, currentPage * rowsPerPage + currentPageRows);
+    }
+
+    if (orderBy === 'desc') {
+      return [...this.allRows].sort((pre, next) => (next[columnId] < pre[columnId] ? -1 : 1))
+        .slice(currentPage * rowsPerPage, currentPage * rowsPerPage + currentPageRows);
+    }
+
+    return [...this.allRows].slice(currentPage * rowsPerPage, currentPage * rowsPerPage + currentPageRows);
+  }
+
+  /**
+   * Call onChangePage callback
+   * @param  {Object} event
+   * @param  {number} page
+   */
+  handleChangePage(event, page) {
     this.setState({
       ...this.state,
-      currentRow,
-      checkedRows,
+      rows: this.getRows(page, this.state.rowsPerPage, {...this.order}),
+      currentPage: page,
     });
+  }
+
+  /**
+   * Call onChangeRowsPerPage callback
+   * @param  {Object} event
+   */
+  handleChangeRowsPerPage(event) {
+    this.setState({
+      ...this.state,
+      rowsPerPage: event.target.value,
+      rows: this.getRows(0, event.target.value, {...this.order}),
+      currentPage: 0,
+    })
+  }
+
+  /**
+   * Call onRowSelect callback
+   * @param  {number}  rowId - Clicked row id.
+   * @param  {Array}  selectedRowIds - An array of selected row ids.
+   * @param  {Object}  event
+   * @param  {Boolean} isChecked
+   */
+  handleRowSelect(rowId, selectedRowIds, event, isChecked) {
+    this.setState({
+      ...this.state,
+      rowId,
+      selectedRowIds,
+    });
+  }
+
+  /**
+   * Call onOrderChange callback
+   * @param  {Object} options
+   * @param  {string} options.columnId - Sort column id.
+   * @param  {string} options.orderBy - Order by 'asc' or 'desc' or false.
+   */
+  handleOrderChange({columnId, orderBy}) {
+    this.order = {
+      columnId,
+      orderBy,
+    };
+
+    this.setState({
+      ...this.state,
+      rows: this.getRows(this.state.currentPage, this.state.rowsPerPage, {...this.order}),
+    })
   }
 
   /**
@@ -122,28 +168,25 @@ export default class table extends React.Component {
    */
   render() {
     const {
-      currentRow,
-      checkedRows,
+      rows,
+      rowId,
+      selectedRowIds,
     } = this.state;
 
     return (
       <div style={{fontSize: '16px'}}>
-        <h1>Selecting</h1>
-        <div>currentRow: {JSON.stringify(currentRow)}</div>
-        <div>checkedRows: {JSON.stringify(checkedRows)}</div>
+        <div>rowId: {JSON.stringify(rowId)}</div>
+        <div>selectedRowIds: {JSON.stringify(selectedRowIds)}</div>
+        <div>Note: The sort order is according to string Unicode code points.</div>
         <Table
+          total={this.total}
           columns={this.columns}
-          rows={this.data}
-          onCheckedChange={this.onCheckedChange.bind(this)}
-        />
-        <h1>Sorting & Selecting</h1>
-        <Table
-          columns={this.columns}
-          rows={this.data}
-          order={{
-            columnId: 'name',
-            orderBy: 'desc',
-          }}
+          rows={rows}
+          order={this.order}
+          onChangePage={this.handleChangePage.bind(this)}
+          onChangeRowsPerPage={this.handleChangeRowsPerPage.bind(this)}
+          onRowSelect={this.handleRowSelect.bind(this)}
+          onOrderChange={this.handleOrderChange.bind(this)}
         />
       </div>
     );
