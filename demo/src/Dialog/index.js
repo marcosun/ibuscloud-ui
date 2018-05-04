abb7afc40c694f38a31669015ac0b00d978837e6
marcosun <marcoinbristol@gmail.com>
2018-05-04 15:32:02 +0800

    [demo] Create a button to open Dialog, click on background to close Dialog

--- demo/src/Dialog/index.js
+++ demo/src/Dialog/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
 import {withStyles} from 'material-ui/styles';
+import Button from 'material-ui/Button';
 
 import {default as IBusUiDialog} from 'ibuscloud-ui/Dialog';
 
@@ -13,12 +14,50 @@ const styles = (theme) => ({
 @hot(module)
 @withStyles(styles)
 class Dialog extends React.Component {
+  state = {
+    isOpen: false,
+  };
+
+  /**
+   * Open dialog when button is clicked
+   */
+  handleOpenDialog() {
+    this.setState({
+      isOpen: true,
+    });
+  }
+
+  /**
+   * Close dialog when Dialog fires onCose event
+   */
+  handleCloseDialog() {
+    this.setState({
+      isOpen: false,
+    });
+  }
+
   /**
    * @return {Component}
    */
   render() {
+    const {
+      isOpen,
+    } = this.state;
+
     return (
-      <div></div>
+      <div>
+        <Button
+          color='primary'
+          variant='raised'
+          onClick={this.handleOpenDialog.bind(this)}
+        >
+          Open Dialog
+        </Button>
+        <IBusUiDialog
+          open={isOpen}
+          onClose={this.handleCloseDialog.bind(this)}
+        />
+      </div>
     );
   }
 }
