c55d7bd703d9dc902b129b58f7ddb7896dea8f1d
marcosun <marcoinbristol@gmail.com>
2018-05-04 15:30:11 +0800

    [Dialog] A very basic Dialog which passes all props to MuiDialog

--- src/Dialog/Dialog.js
+++ src/Dialog/Dialog.js
@@ -1,6 +1,44 @@
 import React from 'react';
+import {
+  object,
+} from 'prop-types';
+import {withStyles} from 'material-ui/styles';
+import {
+  default as MuiDialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog';
 
+const styles = (theme) => {
+  return {
+  };
+};
+
+/**
+ * Dialog
+ */
+@withStyles(styles, {name: 'IBusUiDialog'})
 class Dialog extends React.PureComponent {
+  static propTypes = {
+    classes: object,
+  };
+
+  /**
+   * @return {Component}
+   */
+  render() {
+    const {
+      classes,
+      ...others
+    } = this.props;
+
+    return (
+      <MuiDialog {...others}>
+      </MuiDialog>
+    );
+  }
 }
 
 export default Dialog;
