772649d39b8db6d64df0c872191e002560182555
marcosun <marcoinbristol@gmail.com>
2018-05-04 14:43:58 +0800

    [demo] Create Dialog demo boilerplate

---
+++ demo/src/Dialog/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {hot} from 'react-hot-loader';
+import {withStyles} from 'material-ui/styles';
+
+import {default as IBusUiDialog} from 'ibuscloud-ui/Dialog';
+
+const styles = (theme) => ({
+});
+
+/**
+ * Dialog demo
+ */
+@hot(module)
+@withStyles(styles)
+class Dialog extends React.Component {
+  /**
+   * @return {Component}
+   */
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Dialog;
