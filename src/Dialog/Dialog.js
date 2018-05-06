5199fe52fc5f4266a902d96d258020e5da1df096
marcosun <marcoinbristol@gmail.com>
2018-05-06 20:17:14 +0800

    [Dialog] Add title to the Dialog

--- src/Dialog/Dialog.js
+++ src/Dialog/Dialog.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  string,
   object,
 } from 'prop-types';
 import {withStyles} from 'material-ui/styles';
@@ -10,19 +11,31 @@ import {
   DialogContentText,
   DialogTitle,
 } from 'material-ui/Dialog';
+import Typography from 'material-ui/Typography';
 
-const styles = (theme) => {
-  return {
-  };
-};
+const styles = (theme) => ({
+  dialog: {
+    minWidth: 640,
+    minHeight: 216,
+  },
+  title: {
+    fontSize: 22,
+  },
+});
 
 /**
  * Dialog
+ * @param {string} [props.title=''] - Dialog title
  */
 @withStyles(styles, {name: 'IBusUiDialog'})
 class Dialog extends React.PureComponent {
   static propTypes = {
     classes: object,
+    title: string,
+  };
+
+  static defaultProps = {
+    title: '',
   };
 
   /**
@@ -31,11 +44,31 @@ class Dialog extends React.PureComponent {
   render() {
     const {
       classes,
+      title,
       ...others
     } = this.props;
 
     return (
-      <MuiDialog {...others}>
+      <MuiDialog
+        classes={{
+          paper: classes.dialog,
+        }}
+        {...others}
+      >
+        <DialogTitle disableTypography={true}>
+          <Typography
+            classes={{
+              title: classes.title,
+            }}
+            variant="title"
+          >
+            {title}
+          </Typography>
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+          </DialogContentText>
+        </DialogContent>
       </MuiDialog>
     );
   }
