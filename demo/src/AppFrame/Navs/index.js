7d3309352c21038d404b6314e911bb4e70b7034a
余珩 <1281005689@qq.com>
2019-01-22 18:06:40 +0800

    [Demo] Create navs module

---
+++ demo/src/AppFrame/Navs/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import classNames from 'classnames';
+import { Icon, Menu } from 'antd';
+import { Link } from 'react-router-dom';
+import classes from './index.less';
+import logo from './logo.png';
+
+/* Navigate items configuration. */
+const navs = [];
+
+/* Test path against current pathname. */
+function isPathMatchCurrentLocation(path) {
+  /* Path ends with query string is understood as a match. */
+  return new RegExp(`^\\${path}(\\?|$)`).test(window.location.pathname);
+}
+
+/* Navs is a functional component so that it will get re-rendered whenever route changes. */
+function Navs() {
+  /* Matched navigate item will be highlighted. */
+  const currentNavItem = navs.find((navItem) => {
+    return isPathMatchCurrentLocation(navItem.path);
+  });
+
+  /* Select the route page with the path. */
+  const selectedKeys = currentNavItem !== void 0 ? [currentNavItem.path] : [];
+
+  /* ant-layout-header sets logo height the same as header. */
+  const logoClassName = classNames('ant-layout-header', classes.logo);
+
+  return (
+    <React.Fragment>
+      <div className={logoClassName}>
+        <a>
+          <img alt="logo" src={logo} />
+          <h1>公交云</h1>
+        </a>
+      </div>
+      <Menu mode="inline" selectedKeys={selectedKeys} theme="dark">
+        {navs.map((navItem) => (
+          /* Iterate and render navigate items. */
+          <Menu.Item key={navItem.path}>
+            <Link to={navItem.path}>
+              <Icon type={navItem.icon} />
+              <span>{navItem.name}</span>
+            </Link>
+          </Menu.Item>
+        ))}
+      </Menu>
+    </React.Fragment>
+  );
+}
+
+export default hot(module)(Navs);
--- demo/src/AppFrame/index.js
+++ demo/src/AppFrame/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
 import Layout from 'ibuscloud-ui/Layout';
+import Navs from './Navs';
 import Router from '../router';
 
 function AppFrame(props) {
-  return <Layout contentComponent={<Router />} />;
+  return <Layout contentComponent={<Router />} siderComponent={<Navs />} />;
 }
 
 AppFrame.propTypes = {
