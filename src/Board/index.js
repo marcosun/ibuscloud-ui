56e01129b6252df4c2e94970cdd8d53c79dbbf7b
余珩 <1281005689@qq.com>
2019-01-23 13:52:56 +0800

    [core] Board

---
+++ src/Board/index.js
@@ -0,0 +1,7 @@
+import withRootStyle from '../styles/withRootStyle';
+import classes from './index.less';
+
+/**
+ * Used in Header to wrap BreadCrumb, title and inputs.
+ */
+export default withRootStyle(classes.root);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as BarChart } from './BarChart';
+export { default as Board } from './Board';
 export { default as BreadcrumbWithTitle } from './BreadcrumbWithTitle';
 export { default as Layout } from './Layout';
